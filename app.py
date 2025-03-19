fdee2b5142fb7c8c0bdb1bbd4d4272ece1c7a1ee
prelovac-milenko <prelovacmilenkofpe@gmail.com>
2025-03-19 20:10:38 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,56 +4,70 @@ import asyncio
 
 app = FastAPI()
 
-# Omogućavanje pristupa sa drugih uređaja
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-voting_data = {"question": "Čekam unos pitanja...", "yes": 0, "no": 0}
+
+voting_data = {"question": "Čekam unos...", "options": {}, "votes": {}}
 connections = []
 
-async def broadcast():
-    """Šalje ažurirane podatke svim povezanim klijentima."""
-    if connections:
-        for conn in connections:
-            try:
-                await conn.send_json(voting_data)
-            except:
-                connections.remove(conn)
 
-@app.get("/question")
-async def get_question():
-    return voting_data
+@app.post("/set_question/")
+async def set_question(data: dict):
 
-@app.post("/set_question/{question}")
-async def set_question(question: str):
-    voting_data["question"] = question
-    voting_data["yes"] = 0
-    voting_data["no"] = 0
+    voting_data["question"] = data["question"]
+    voting_data["options"] = data["options"]
+    voting_data["votes"] = {opt: 0 for opt in data["options"]}
+
     await broadcast()
     return {"message": "Pitanje postavljeno"}
 
+
+@app.post("/cancel_question/")
+async def cancel_question():
+
+    voting_data["question"] = "Trenutno nema pitanja..."
+    voting_data["options"] = {}
+    voting_data["votes"] = {}
+
+    await broadcast()
+    return {"message": "Pitanje obrisano"}
+
+
 @app.post("/vote/{option}")
 async def vote(option: str):
-    if option in ["yes", "no"]:
-        voting_data[option] += 1
+
+    if option in voting_data["votes"]:
+        voting_data["votes"][option] += 1
         await broadcast()
-        return {"message": "Glas prihvaćen"}
-    return {"error": "Nevažeća opcija"}
+    return {"message": "Glas zabeležen"}
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """ WebSocket za ažuriranje podataka u realnom vremenu """
     await websocket.accept()
     connections.append(websocket)
-    await websocket.send_json(voting_data)  # Odmah šalje trenutno stanje
+    await websocket.send_json(voting_data)
+
     try:
         while True:
-            await asyncio.sleep(1)  # Drži konekciju otvorenom
+            await asyncio.sleep(1)
             await websocket.send_json(voting_data)
     except:
         connections.remove(websocket)
 
 
+async def broadcast():
+    """ Šalje nove podatke svim povezanim racunarima """
+    for connection in connections:
+        try:
+            await connection.send_json(voting_data)
+        except:
+            connections.remove(connection)
